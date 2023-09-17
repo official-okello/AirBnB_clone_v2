05d537a969e489dbecf0f489e2925621802f8f9f
official-okello <officialokello@gmail.com>
2023-09-17 12:13:38 +0300

    Prepares mysql server

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Creates hbnb_test_db database
+-- Privileges for new user (hbnb_test)
+-- Prepares mysql server
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
