9a0e40ab03055f6b0931801447e14cde2018927c
official-okello <officialokello@gmail.com>
2023-09-20 23:21:26 +0300

    Quick fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-import sys
 from datetime import datetime
+import sys
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -17,14 +18,13 @@ class BaseModel:
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
+        self.id = str(uuid.uuid4())
         if not kwargs:
             from models import storage
 
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
         else:
             if kwargs.get("created_at"):
                 kwargs["created_at"] = datetime.strptime(
@@ -62,10 +62,10 @@ class BaseModel:
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
+
         return dictionary
 
     def delete(self):
-        """Delete the current instance from storage"""
+        """delete the current instance from storage"""
         from models import storage
-
-        ==storage.delete(self)
+        storage.delete(self)
